f68ad0d460e6c312162ea8ad89d09a67aaccf82b
Grant Stampfli <mail@gstampfli.com>
2014-06-12 14:22:25 -0700

    google maps api set up, need to make address plug in work

--- script.js
+++ script.js
@@ -48,7 +48,16 @@ function generateTitle (array) {
 		$('div.title').append(array.venue.name);
 	}
 
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
 
-AIzaSyCiWXjY3kc8sPKIqlH_4xnZDqZ5TPxGGq0
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
