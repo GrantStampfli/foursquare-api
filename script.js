5f43b71b3354289ebb6967369b0a50339a52093d
Tian Song <tian.y.song@gmail.com>
2014-06-12 20:47:13 -0700

    takes into account the users location. does not work in chrome. no error handling just yet

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 $(document).ready(function() {
 	'use strict';
-	var location = getLocation();
-	console.log(location);
-	var url = 'https://api.foursquare.com/v2/venues/explore?';
-	var fourSquareOptions = {
-    'near': 'Portland, OR',
- 		'client_id': 'ZEFCPOZI0JUGL1H2IKFMHO1PLHSWCOKKZSDOUSPHYQ2QHNEO',
-		'client_secret': 'I214RPZ3LOUZXYXAESRLBGZRJCVI2J1EKYPVSOGPPCH5RZQN',
-		'v': '20140612',
-		'radius': '2000',
-		'section': 'food',
-		'limit': '100',
-		'venuePhotos': '1'
-	};
-	$.ajax(url, { dataType: 'jsonp', data: fourSquareOptions})
-	.then(function(data, status, xhr) {
-		console.log(status);
-		console.log('success (promises): ' + data);
-		var dataArray = data.response.groups[0].items;
+	if ("geolocation" in navigator) {
+		console.log('good to go');
+	} else {
+	  console.log('shit is going down');
+	}
+	
+	navigator.geolocation.getCurrentPosition(function(position) {
+		var ll = position.coords.latitude + ',' +position.coords.longitude;
+  	console.log(ll);
 
-		var rngNumb = Math.floor((Math.random() * 100));
-		var placeArray = data.response.groups[0].items[rngNumb];
-		generatePics(generatePicUrl(placeArray));
-		generateTitle(placeArray);
-		var latitude = placeArray.venue.location.lat;
-		var longitude =placeArray.venue.location.lng;
-		console.log(latitude, longitude);
-		google.maps.event.addDomListener(window, 'load', initialize(latitude, longitude));
+		var url = 'https://api.foursquare.com/v2/venues/explore?';
+		var fourSquareOptions = {
+			'll': ll,
+	    // 'near': 'Portland, OR',
+	 		'client_id': 'ZEFCPOZI0JUGL1H2IKFMHO1PLHSWCOKKZSDOUSPHYQ2QHNEO',
+			'client_secret': 'I214RPZ3LOUZXYXAESRLBGZRJCVI2J1EKYPVSOGPPCH5RZQN',
+			'v': '20140612',
+			'radius': '1000',
+			'section': 'food',
+			'limit': '20',
+			'venuePhotos': '1'
+		};
+		$.ajax(url, { dataType: 'jsonp', data: fourSquareOptions})
+		.then(function(data, status, xhr) {
+			console.log(status);
+			console.log('success (promises): ' + data);
+			var dataArray = data.response.groups[0].items;
 
-	}, function(xhr, status, error) {
-		console.log(status);
-		console.log('failed (promises): ' + error);
+			var rngNumb = Math.floor((Math.random() * dataArray.length));
+			var placeArray = data.response.groups[0].items[rngNumb];
+			generatePics(generatePicUrl(placeArray));
+			generateTitle(placeArray);
+			var latitude = placeArray.venue.location.lat;
+			var longitude =placeArray.venue.location.lng;
+			google.maps.event.addDomListener(window, 'load', initialize(latitude, longitude));
+		}, function(xhr, status, error) {
+			console.log(status);
+			console.log('failed (promises): ' + error);
+		});
 	});
 });
 
@@ -50,28 +58,22 @@ function generateTitle (array) {
 		// $('div.title').append(array.venue.name);
 	}
 
-var map;
 function initialize(lat, lng) {
   var myLatlng = new google.maps.LatLng(lat, lng);
-var mapOptions = {
-  zoom: 16,
-  center: myLatlng
-};
-var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	var mapOptions = {
+	  zoom: 16,
+	  center: myLatlng
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-var marker = new google.maps.Marker({
-    position: myLatlng,
-    title:""
-});
-
-
-marker.setMap(map);
-  }
-var getLocation = function (argument) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-  return (position.coords.latitude, position.coords.longitude);
+	var marker = new google.maps.Marker({
+	    position: myLatlng,
+	    title:""
 	});
-};
+	marker.setMap(map);
+}
+
+	
 
 
 
